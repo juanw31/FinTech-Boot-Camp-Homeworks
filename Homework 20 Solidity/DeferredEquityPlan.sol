8fcddc60e94a98aff1789915eaa7f79b67601398
juanw31 <juanw31@gmail.com>
2021-07-13 19:32:17 -0700

    add a file

--- Homework 20 Solidity/DeferredEquityPlan.sol
+++ Homework 20 Solidity/DeferredEquityPlan.sol
@@ -1,105 +1,72 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "pragma solidity ^0.5.0;\n",
-    "\n",
-    "// lvl 3: equity plan\n",
-    "contract DeferredEquityPlan {\n",
-    "    \n",
-    "    uint fakenow = now;\n",
-    "    \n",
-    "    function fastforward() public {\n",
-    "       fakenow += 100 days;\n",
-    "    }\n",
-    "    \n",
-    "    address human_resources;\n",
-    "\n",
-    "    address payable employee; // bob\n",
-    "    bool active = true; // this employee is active at the start of the contract\n",
-    "\n",
-    "    // @TODO: Set the total shares and annual distribution\n",
-    "    // Your code here!\n",
-    "    \n",
-    "    uint total_shares = 1000;\n",
-    "    uint annual_distribution = 250;\n",
-    "\n",
-    "    uint start_time = fakenow; // permanently store the time this contract was initialized\n",
-    "\n",
-    "    // @TODO: Set the `unlock_time` to be 365 days from now\n",
-    "    uint unlock_time = fakenow + 365 days;\n",
-    "\n",
-    "    uint public distributed_shares; // starts at 0\n",
-    "\n",
-    "    constructor(address payable _employee) public {\n",
-    "        human_resources = msg.sender;\n",
-    "        employee = _employee;\n",
-    "    }\n",
-    "\n",
-    "    function distribute() public {\n",
-    "        require(msg.sender == human_resources || msg.sender == employee, \"You are not authorized to execute this contract.\");\n",
-    "        require(active == true, \"Contract not active.\");\n",
-    "\n",
-    "        // @TODO: Add \"require\" statements to enforce that:\n",
-    "        // 1: `unlock_time` is less than or equal to `now`\n",
-    "        // 2: `distributed_shares` is less than the `total_shares`\n",
-    "        // Your code here!\n",
-    "        \n",
-    "        require(unlock_time <= fakenow, \"Shares is not avalible at this time.\");\n",
-    "        require(distributed_shares < total_shares, \"no more share to be distributed.\");\n",
-    "\n",
-    "        // @TODO: Add 365 days to the `unlock_time`\n",
-    "        unlock_time += 365;\n",
-    "\n",
-    "        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution\n",
-    "        // Make sure to include the parenthesis around (now - start_time) to get accurate results!\n",
-    "        // Your code here!\n",
-    "        \n",
-    "        distributed_shares = (fakenow - start_time) / 365 * annual_distribution;\n",
-    "\n",
-    "        // double check in case the employee does not cash out until after 5+ years\n",
-    "        if (distributed_shares > 1000) {\n",
-    "            distributed_shares = 1000;\n",
-    "        }\n",
-    "    }\n",
-    "\n",
-    "    // human_resources and the employee can deactivate this contract at-will\n",
-    "    function deactivate() public {\n",
-    "        require(msg.sender == human_resources || msg.sender == employee, \"You are not authorized to deactivate this contract.\");\n",
-    "        active = false;\n",
-    "    }\n",
-    "\n",
-    "    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly\n",
-    "    function() external payable {\n",
-    "        revert(\"Do not send Ether to this contract!\");\n",
-    "    }\n",
-    "}"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
+pragma solidity ^0.5.0;
+
+// lvl 3: equity plan
+contract DeferredEquityPlan {
+    
+    uint fakenow = now;
+    
+    function fastforward() public {
+       fakenow += 100 days;
+    }
+    
+    address human_resources;
+
+    address payable employee; // bob
+    bool active = true; // this employee is active at the start of the contract
+
+    // @TODO: Set the total shares and annual distribution
+    // Your code here!
+    
+    uint total_shares = 1000;
+    uint annual_distribution = 250;
+
+    uint start_time = fakenow; // permanently store the time this contract was initialized
+
+    // @TODO: Set the `unlock_time` to be 365 days from now
+    uint unlock_time = fakenow + 365 days;
+
+    uint public distributed_shares; // starts at 0
+
+    constructor(address payable _employee) public {
+        human_resources = msg.sender;
+        employee = _employee;
+    }
+
+    function distribute() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
+        require(active == true, "Contract not active.");
+
+        // @TODO: Add "require" statements to enforce that:
+        // 1: `unlock_time` is less than or equal to `now`
+        // 2: `distributed_shares` is less than the `total_shares`
+        // Your code here!
+        
+        require(unlock_time <= fakenow, "Shares is not avalible at this time.");
+        require(distributed_shares < total_shares, "no more share to be distributed.");
+
+        // @TODO: Add 365 days to the `unlock_time`
+        unlock_time += 365;
+
+        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
+        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
+        // Your code here!
+        
+        distributed_shares = (fakenow - start_time) / 365 * annual_distribution;
+
+        // double check in case the employee does not cash out until after 5+ years
+        if (distributed_shares > 1000) {
+            distributed_shares = 1000;
+        }
+    }
+
+    // human_resources and the employee can deactivate this contract at-will
+    function deactivate() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        active = false;
+    }
+
+    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
+    function() external payable {
+        revert("Do not send Ether to this contract!");
+    }
 }
